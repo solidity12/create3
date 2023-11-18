03c694bdddce2d5df25781f3ee50d208668d871e
mg12 <volibear1001@gmail.com>
2023-11-18 14:30:24 +0900

    chore: Add event to IFactory contract

--- contracts/interfaces/IFactory.sol
+++ contracts/interfaces/IFactory.sol
@@ -9,6 +9,12 @@ import "./IDestruct.sol";
  */
 interface IFactory is IDestruct {
     /**
+     * @dev Emitted when a new implement contract is created.
+     * @param implement Address of the created implement contract.
+     */
+    event ImplementCreated(address indexed implement);
+
+    /**
      * @dev Deploys a new Implement contract.
      * @param implementBytecode The bytecode of the Implement contract to be deployed.
      * @return implement The address of the newly deployed Implement contract.
