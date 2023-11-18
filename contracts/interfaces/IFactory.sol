23443cd24751fc6b59d2c582b9100d34cb651b22
mg12 <volibear1001@gmail.com>
2023-11-18 14:25:35 +0900

    chore: Add comments to IFactory contract

--- contracts/interfaces/IFactory.sol
+++ contracts/interfaces/IFactory.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.9;
 
 import "./IDestruct.sol";
 
+/**
+ * @title IFactory Interface
+ * @dev Interface for the Factory contract.
+ */
 interface IFactory is IDestruct {
+    /**
+     * @dev Deploys a new Implement contract.
+     * @param implementBytecode The bytecode of the Implement contract to be deployed.
+     * @return implement The address of the newly deployed Implement contract.
+     */
     function create1(
-        bytes memory _implementBytecode
-    ) external returns (address _implement);
+        bytes memory implementBytecode
+    ) external returns (address implement);
 }
