4671aceaa1f8d59b6b4bd6dcb1e1048768a24d47
mg12 <volibear1001@gmail.com>
2023-11-18 14:14:38 +0900

    chore: Add Comments to Destruct Contract

--- contracts/interfaces/IDestruct.sol
+++ contracts/interfaces/IDestruct.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/**
+ * @title IDestruct Interface
+ * @dev Interface for the Destruct contract.
+ */
 interface IDestruct {
+    /**
+     * @dev Destructs the Factory and Implement contracts.
+     * This function triggers the self-destruct.
+     */
     function destruct() external;
 }
